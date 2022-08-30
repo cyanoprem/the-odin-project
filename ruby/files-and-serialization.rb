5695a3fdce9dd1fbce306c197264febd2da5400a
Cyano Prem <cyano.prem@gmail.com>
2022-08-30 19:11:17 +0530

    files and serialization

--- ruby/files-and-serialization.rb
+++ ruby/files-and-serialization.rb
@@ -165,3 +165,16 @@ class People
     @persons << person
   end
 end
+
+
+# Writing to a local file from a webpage
+
+require 'rubygems'
+require 'rest-client'
+
+wiki_url = "http://en.wikipedia.org/"
+wiki_local_filename = "wiki-page.html"
+
+File.open(wiki_local_filename, "w") do |file|
+   file.write(RestClient.get(wiki_url))
+end
